74bd16a93003da9ce49bbe391013372d8d218780
arbiva <arbiva@post.bgu.ac.il>
2022-07-16 12:36:17 +0300

    before deploy

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Thumbnail from './Thumbnail'
+import React from "react";
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 function Results({ results }) {
   return (
-    <div>
-      {results.map(result => (
-        <Thumbnail key={result.id} result={result}/>
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex justify-center">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
-  )
+    </FlipMove>
+  );
 }
 
-export default Results
+export default Results;
